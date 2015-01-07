89d46c2a72c241f00208b419004be0c6664e7e58
Matt Petrie <matthew.petrie@gmail.com>
2015-01-07 01:29:47 -0800

    create watchify gulp task and add to watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var config = require('../config');
 
-gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('./src/views/**', ['markup']);
-  gulp.watch('./src/stylesheets/*.{sass,scss}', ['sass']);
+gulp.task('watch', ['watchify', 'browser-sync'], function() {
+  gulp.watch(config.markup.src, ['markup']);
+  gulp.watch(config.sass.src, ['sass']);
 });
---
+++ gulp/tasks/watchify.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserifyTask = require('./browserify');
+
+gulp.task('watchify', function(callback) {
+  browserifyTask(callback, true);
+});
