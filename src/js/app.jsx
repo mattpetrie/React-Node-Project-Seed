43773db9278a998f746f2f43fbc5945ce4bfb3e2
Matt Petrie <matthew.petrie@gmail.com>
2015-01-13 14:56:11 -0800

    move todo app component logic into own file

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,61 +1,6 @@
 var React = require('react');
-var $ = require('jquery');
-var _ = require('lodash');
 
-var TodoList = require('./components/todo-list.jsx');
-
-var TodoApp = React.createClass({
-  getInitialState: function() {
-    return ({ data: [] });
-  },
-  loadTodosFromServer: function() {
-    $.ajax({
-      url: '/api/todos',
-      dataType: 'json',
-      success: function(data) {
-        this.setState({ data: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error('/api/todos', status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleTodoUpdate: function(todo) {
-    this.updateTodo(todo);
-
-    $.ajax({
-      url: '/api/todos/' + todo._id,
-      type: 'PUT',
-      dataType: 'json',
-      data: todo,
-      success: function() {
-        this.loadTodosFromServer();
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.err('/api/todos' + todo._id, status, err.toString());
-        this.loadTodosFromServer();
-      }.bind(this)
-    });
-  },
-  updateTodo: function(updatedTodo) {
-    var todos = this.state.data;
-    var todoToUpdateIndex = _.findIndex(todos, function(todo) {
-      return todo._id === updatedTodo._id;
-    });
-    _.merge(todos[todoToUpdateIndex], updatedTodo);
-    this.setState({ data: todos });
-  },
-  componentDidMount: function() {
-    this.loadTodosFromServer();
-  },
-  render: function() {
-    return (
-      <div className="todoApp">
-        <TodoList data={this.state.data} handleTodoUpdate={this.handleTodoUpdate} />
-      </div>
-    );
-  }
-});
+var TodoApp = require('./components/todo-app.jsx');
 
 React.render(
   <TodoApp />, document.getElementById('content')
---
+++ src/js/components/todo-app.jsx
@@ -0,0 +1,65 @@
+var React = require('react');
+var $ = require('jquery');
+var _ = require('lodash');
+
+var TodoList = require('./todo-list.jsx');
+
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return ({ data: [] });
+  },
+  loadTodosFromServer: function() {
+    $.ajax({
+      url: '/api/todos',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('/api/todos', status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleTodoUpdate: function(todo) {
+    this.updateTodo(todo);
+
+    $.ajax({
+      url: '/api/todos/' + todo._id,
+      type: 'PUT',
+      dataType: 'json',
+      data: todo,
+      success: function() {
+        this.loadTodosFromServer();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.err('/api/todos' + todo._id, status, err.toString());
+        this.loadTodosFromServer();
+      }.bind(this)
+    });
+  },
+  updateTodo: function(updatedTodo) {
+    var todos = this.state.data;
+    var todoToUpdateIndex = _.findIndex(todos, function(todo) {
+      return todo._id === updatedTodo._id;
+    });
+    if (todoToUpdateIndex === -1) {
+      throw new Error('Todo ' + updatedTodo + '  not found in todos collection');
+    }
+    if (!_.isEqual(todos[todoToUpdateIndex], updatedTodo)) {
+      _.merge(todos[todoToUpdateIndex], updatedTodo);
+      this.setState({ data: todos });
+    }
+  },
+  componentDidMount: function() {
+    this.loadTodosFromServer();
+  },
+  render: function() {
+    return (
+      <div className="todoApp">
+        <TodoList data={this.state.data} handleTodoUpdate={this.handleTodoUpdate} />
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
