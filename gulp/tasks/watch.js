40cb068272dab7e655964ec13a39b3af577634c9
Matt Petrie <matthew.petrie@gmail.com>
2015-01-15 13:43:51 -0800

    Fix gulp watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,9 @@
 var gulp = require('gulp');
 var config = require('../config');
 
-gulp.task('watch', ['watchify', 'browser-sync'], function() {
+gulp.task('watch', ['watchify', 'browser-sync', '_watch']);
+
+gulp.task('_watch', function() {
   gulp.watch(config.markup.src, ['markup']);
   gulp.watch(config.sass.src, ['sass']);
 });
