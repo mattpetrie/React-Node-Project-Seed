63a30903b67d71594ff314f50603b99081e2a881
Matt Petrie <matthew.petrie@gmail.com>
2015-01-23 00:34:38 -0800

    Add Mocha-style reporting to Karma

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,10 @@ module.exports = function(config) {
         // list of files to exclude
         exclude: [],
 
+        mochaReporter: {
+          output: 'autowatch',
+        },
+        
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
@@ -39,7 +43,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['mocha'],
 
         // web server port
         port: 9876,
