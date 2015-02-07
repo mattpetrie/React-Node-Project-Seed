e16e00a0916c85562cef1e845583ba43f31d0cbf
Matt Petrie <matthew.petrie@gmail.com>
2015-02-07 02:34:17 -0800

    Install React Router

---
+++ src/js/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react/addons';
+import Router from 'react-router';
+import routes from './routes';
+
+React.initializeTouchEvents(true);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  Router.run(routes, (Handler, state) => {
+    React.render(<Handler />, document.querySelector('#content'));
+  });
+});
