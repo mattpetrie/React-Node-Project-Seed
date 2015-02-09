d9f3770f25a0e71099965a50e4e508e315d945ba
Matt Petrie <matthew.petrie@gmail.com>
2015-02-09 00:05:46 -0800

    rename development MongoDB database

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production'){
   mongoose.connect(mongooseUri, options);
 
 } else {
-  mongoose.connect('localhost', 'test');
+  mongoose.connect('localhost', 'react-node-project-seed-development');
 }
 
 const db = mongoose.connection;
