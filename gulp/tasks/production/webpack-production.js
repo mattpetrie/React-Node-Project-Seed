1deb1feff8704810e81afc031b3c362f473ef7fd
Matt Petrie <matthew.petrie@gmail.com>
2015-02-08 17:58:21 -0800

    Don't use react-hot loader in production

--- gulp/tasks/production/webpack-production.js
+++ gulp/tasks/production/webpack-production.js
@@ -6,15 +6,6 @@ var assign = require('object-assign');
 
 gulp.task('webpack:production', ['clean'], function(callback){
   productionConfig = assign({}, webpackConfig, {
-    module: {
-      loaders: [
-        { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['6to5-loader'] },
-        { test: /\.css$/, loader: 'style-loader!css-loader!autoprefixer-loader?last 2 version' },
-        { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader?includePaths[]=' +
-            path.resolve(__dirname, './src/stylesheets') +
-            '!autoprefixer-loader?browsers=last 2 version' },
-      ],
-    },
     plugins: [
       new webpack.optimize.UglifyJsPlugin(),
       new webpack.optimize.DedupePlugin()
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// Don't use react-hot loader in production
+var jsxLoaders = process.env.NODE_ENV === 'production' ? ['6to5-loader'] : ['react-hot', '6to5-loader'];
+
 module.exports = {
   entry: {
     main: ['./src/js/main.jsx'],
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', '6to5-loader'] },
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: jsxLoaders },
       { test: /\.css$/, loader: 'style-loader!css-loader!autoprefixer-loader?last 2 version' },
       { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader?includePaths[]=' +
           path.resolve(__dirname, './src/stylesheets') +
