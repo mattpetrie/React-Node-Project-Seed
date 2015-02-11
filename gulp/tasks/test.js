176c6a51cbae17b09cef62332937b688b42db8c6
Matt Petrie <matthew.petrie@gmail.com>
2015-02-10 21:40:46 -0800

    setup mocha testing and gulp tests for server code

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 var karma = require('karma').server;
 var path = require('path');
 
-gulp.task('test', function(done) {
+gulp.task('test', ['test:client', 'test:server']);
+
+gulp.task('test:watch', ['test:client:watch', 'test:server:watch']);
+
+gulp.task('test:client', function(done) {
   karma.start({
     configFile: path.resolve(__dirname, '../../karma.conf.js'),
     singleRun: true
   }, done);
 });
 
-gulp.task('test:watch', function(done) {
+gulp.task('test:client:watch', function(done) {
   karma.start({
     configFile: path.resolve(__dirname, '../../karma.conf.js'),
   }, done);
 });
+
+gulp.task('test:server', shell.task([
+  'echo Running Server Tests',
+  'mocha'
+]));
+
+gulp.task('test:server:watch', shell.task([
+  'echo Running Server Tests with Watch',
+  'mocha --watch'
+]));
